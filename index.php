98a8287845b5be7fbf9ce5e7472c220c2d0af981
jhonatan1153 <jhonatan1153@hotmail.com>
2019-08-12 16:24:41 -0500

    liso responsive

--- index.php
+++ index.php
@@ -6,11 +6,18 @@
    
     <?php
 
-    $nombre = $_POST['nombre'];
-    
-    echo $nombre;
-    
+    error_reporting(0);
+
+     $nombre = $_POST["nombre"];
+     $apellido = $_POST["apellido"];
+     $correo = $_POST["correo"];
+     $pass = $_POST["pass"];
+   
+ 
     ?>
+<style>
+
+  </style>
     <head>
             <title>ROCIO</title>
             
@@ -18,29 +25,48 @@
         <link rel="stylesheet" type="text/css" href="bootstrap/superior.css">
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 
-        
+   
             
     </head>
     <body>
   
+    
 
-    <div class="superior"> 
+    <header class="superior"> 
+
+   
+
             <div class="btn-group">
                    <a class="button" href="#">inicio</a>
                     <a class="button" href="catalogo.html">nuestro catalogo</a>
                     <a class="button" href="#">contactenos</a>   
                     <a class="button" href="registro.html" >registro</a>
-                    
+                    <a class="button" href="login.html" >login</a>  
              </div>
-             <a class ="img" href=""></a>    
 
-    </div>
+             <a class ="img" href=""></a>
+
+
+             <span class="despegable">
+                    <button class="boton">MENU</button>
+                      <div class="links">
+                        <a href="#">inicio</a>
+                          <a href="catalogo.html">nuestro catalogo</a>
+                          <a href="#">contactenos</a>   
+                          <a href="registro.html" >registro</a>
+                          <a href="login.html" >login</a>  
+                      <div>
+                </span>
+
+                
+    </header>
+    
     </body>
 
     <div class="slider">
 			<ul>
 				<li>
-  <img src="public/images/banner.jpg" style="height: 500px; " alt="">
+  <img src="public/images/banner.jpg" style="height: 500px; width: 100%;" alt="">
  </li>
  
 				<li>
