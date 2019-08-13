ab700d4352c93f02372a50b6ab70fe09fb681853
jhonatan1153 <jhonatan1153@hotmail.com>
2019-08-13 16:44:19 -0500

    listo

--- catalogo.php
+++ catalogo.php
@@ -10,10 +10,9 @@
             <div class="superior"> 
                     <div class="btn-group">
                            <a class="button" href="index.php">inicio</a>
-                            <a class="button" href="catalogo.php">nuestro catalogo</a>
+                            <a class="button" href="catalogo.php">nuestro productos</a>
                             <a class="button" href="contacto.php">contactenos</a>   
-                            <a class="button" href="registro.php" >registro</a>
-                            <a class="button" href="login.php" >login</a>
+
                      </div>
                      <a class ="img" href=""></a>    
         
