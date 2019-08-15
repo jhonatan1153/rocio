2a0f2364b0115de4d5caccd5679d60a0506d21d7
juniorwilson <54047258+juniorwilson@users.noreply.github.com>
2019-08-14 20:31:46 -0500

    Update catalogo.php

--- catalogo.php
+++ catalogo.php
@@ -2,5 +2,45 @@
 
     <body>
    
+
+<div class="container">
+  <div class="row">
+
+<div class="card col-sm-3" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">VIDRIOS</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="col-sm-1"></div>
+
+<div class="card col-sm-3" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">DIVICIONES DE BAÑO</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="col-sm-1"></div>
+
+<div class="card col-sm-3" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">POLLOS DE COCINA</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+</div>
+</div>
+
+
+
+
     </body>
 </html>
